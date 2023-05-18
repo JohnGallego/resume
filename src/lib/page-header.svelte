136bd29a787d7d43798efaf6a5a71d3f49e95b93
John Gallego <johngallegodev@gmail.com>
2023-05-18 16:41:36 -0400

    HEader

---
+++ src/lib/page-header.svelte
@@ -0,0 +1,44 @@
+<script>
+	// logic goes here
+</script>
+
+<header class="page-header">
+	<div class="page-title">
+		<h1 class="name">John Gallego</h1>
+		<h2 class="job-title">Full-Stack Engineer</h2>
+	</div>
+</header>
+
+<style>
+	.page-header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 1rem 1rem;
+		background-color: var(--color-hg-header);
+	}
+
+	.page-title {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+
+	.name,
+	.job-title {
+		margin: 0;
+	}
+
+	.name {
+		font-size: 1.2rem;
+		font-weight: 300;
+		text-decoration: underline;
+		text-underline-offset: 0.3rem;
+	}
+
+	.job-title {
+        margin-top: 0.5em;
+		font-size: 1rem;
+		font-weight: 200;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	import PageHeader from '$lib/page-header.svelte';
+</script>
+
+<PageHeader />
+
+<slot />
